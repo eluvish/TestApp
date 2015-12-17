195493ad642d14155cc0e5ca059846a8edc3dd15
Eli Luvish <iluvish@gmail.com>
2015-12-17 15:38:54 -0500

    fixed lost table

--- database/migrations/2015_12_09_163410_create_outfits_table.php
+++ database/migrations/2015_12_09_163410_create_outfits_table.php
@@ -12,22 +12,22 @@ class CreateOutfitsTable extends Migration
      */
     public function up()
     {
-        // Schema::create('outfits', function (Blueprint $table) {
-        //     $table->increments('id');
-        //     $table->timestamps();
-        //
-        //     $table->integer('user_id')->unsigned();
-        //     $table->foreign('user_id')->references('id')->on('users');
-        //
-        //     $table->string('top_id')->unsigned();
-        //     $table->foreign('top_id')->references('id')->on('items');
-        //
-        //     $table->string('bottom_id')->unsigned();
-        //     $table->foreign('bottom_id')->references('id')->on('items');
-        //
-        //     $table->string('shoe_id')->unsigned();
-        //     $table->foreign('shoe_id')->references('id')->on('items');
-        // });
+        Schema::create('outfits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('top_id')->unsigned();
+            $table->foreign('top_id')->references('id')->on('items');
+
+            $table->string('bottom_id')->unsigned();
+            $table->foreign('bottom_id')->references('id')->on('items');
+
+            $table->string('shoe_id')->unsigned();
+            $table->foreign('shoe_id')->references('id')->on('items');
+        });
     }
 
     /**
